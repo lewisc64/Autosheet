4c530b67e1cd48e06fb288a4868e0bcdcfd23b07
lewisc64 <lewisbcarpenter_@outlook.com>
2020-02-15 00:33:07 +0000

    add value bumping

--- scripts/controls.js
+++ scripts/controls.js
@@ -34,6 +34,7 @@ class Control {
     this.visible = true;
     this.editable = false;
     this.page = 1;
+    this.bump = 0;
     
     this.affects = [];
   }
@@ -42,20 +43,101 @@ class Control {
     return this.groups.indexOf(name) != -1
   }
   
+  isBumpable() {
+    return false;
+  }
+  
+  addBump(n) {
+    this.bump += n;
+    this.update();
+  }
+  
+  resetBump() {
+    this.bump = 0;
+    this.update();
+  }
+  
+  getBump() {
+    return this.bump;
+  }
+  
   configureElement(elem) {
     elem.id = this.id;
-    elem.disabled = !this.editable;
+    //elem.disabled = !this.editable;
+    if (!this.editable) {
+      elem.style["background"] = "none";
+    }
     elem.setSheetValue(this.value);
     
     for (let group of this.groups) {
       elem.classList.add(group);
     }
     
-    elem.addEventListener("input", updateControlEventHandler);
+    if (this.editable) {
+      elem.addEventListener("input", updateControlEventHandler); 
+    }
+    else {
+      elem.addEventListener("focus", function () {
+        document.activeElement.blur();
+      });
+      elem.style.cursor = this.isBumpable() ? "pointer" : "default";
+    }
     
     let setFontSize = (elem) => {
       elem.style["font-size"] = elem.clientHeight * 0.6 + "px";
     }
+    
+    let bumpDialogOpen = false;
+    let bumpDialogId = elem.id + "-bump-dialog";
+    
+    elem.addEventListener("click", (e) => {
+      if (!this.isBumpable()) {
+        return;
+      }
+      
+      let existingDialogElem = document.getElementById(bumpDialogId);
+      if (existingDialogElem != null) {
+        existingDialogElem.parentElement.removeChild(existingDialogElem);
+      }
+      
+      if (bumpDialogOpen) {
+        bumpDialogOpen = false;
+        return;
+      }
+      bumpDialogOpen = true;
+      
+      let bumpDialog = document.createElement("section");
+      bumpDialog.classList.add("bump-dialog");
+      bumpDialog.classList.add("dialog");
+      bumpDialog.id = bumpDialogId;
+      
+      let colorFunc = () => {
+        if (this.getBump() > 0) {
+          elem.style.color = "blue";
+        } else if (this.getBump() < 0) {
+          elem.style.color = "red";
+        } else {
+          elem.style.color = null;
+        }
+      };
+      
+      for (let buttonInfo of [["-", () => this.addBump(-1)], ["R", () => this.resetBump()], ["+", () => this.addBump(1)]]) {
+        let bumpButton = document.createElement("button");
+        bumpButton.classList.add("bump-button");
+        bumpButton.textContent = buttonInfo[0];
+        bumpButton.addEventListener("click", () => {
+          buttonInfo[1]();
+          colorFunc();
+        });
+        bumpDialog.appendChild(bumpButton);
+      }
+      
+      bumpDialog.style.position = "absolute";
+      bumpDialog.style.left = e.pageX - 50 + "px";
+      bumpDialog.style.top = e.pageY - 50 + "px";
+      
+      document.body.appendChild(bumpDialog);
+    });
     
     new ResizeObserver(() => { setFontSize(elem); }).observe(elem);
   }
@@ -144,9 +226,13 @@ class AbilityModifierControl extends Control {
     this.scoreControlId = controls.score;
   }
   
+  isBumpable() {
+    return true;
+  }
+  
   aggregateValuesIn() {
     let total = getControlById(this.scoreControlId).value;
-    this.value = Math.floor(total / 2) - 5;
+    this.value = Math.floor(total / 2) - 5 + this.getBump();
   }
 }
 
@@ -157,12 +243,16 @@ class CopyControl extends Control {
 }
 
 class SumControl extends Control {
+  isBumpable() {
+    return true;
+  }
+  
   aggregateValuesIn() {
     let total = 0;
     for (let id of this.aggregate) {
       total += getControlById(id).value;
     }
-    this.value = total;
+    this.value = total + this.getBump();
   }
 }
 
@@ -170,6 +260,10 @@ class MapControl extends Control {
   constructor(id, groups, value, control, map) {
     super(id, groups, value, [control]);
     this.map = map;
+  }
+  
+  isBumpable() {
+    return typeof value == "number";
   }
   
   aggregateValuesIn() {
